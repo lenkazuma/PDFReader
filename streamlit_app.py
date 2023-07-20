8bd1976e6dc5d20f6157d462c132427c25ec0217
Zhenzhen <zhenzhen1108@hotmail.com>
2023-07-20 16:39:16 +1000

    Fix comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,23 +20,29 @@ def extract_text_from_table(table):
                 text += cell.text + "\n"
     return text.strip()
 
+
 def main():
-    # Load environment variables from .env file
+    # brief summary
+    llm = OpenAI()
+    chain = load_summarize_chain(llm, chain_type="stuff")
+    chain_large = load_summarize_chain(llm, chain_type="map_reduce")
+    chain_qa = load_qa_chain(llm, chain_type="stuff")
+    chain_large_qa = load_qa_chain(llm, chain_type="map_reduce")
+
+
     load_dotenv()
-    
-    # Configure Streamlit page settings
     st.set_page_config(page_title="PDFReader")
     st.title("PDF & Word Reader ✨")
     
-    # Upload file
+    # upload file
     uploaded_file  = st.file_uploader("Upload your file", type=["pdf", "docx"])
     
     # Initialize session state
     if 'pdf_name' not in st.session_state:
         st.session_state.pdf_name = None
     
-    # Extract the text
-    if uploaded_file  is not None:
+    # extract the text
+    if uploaded_file  is not None :
         file_type = uploaded_file.type
 
         # Clear summary if a new file is uploaded
@@ -48,6 +54,7 @@ def main():
         try:
             if file_type == "application/pdf":
                 # Handle PDF files
+                
                 pdf_reader = PdfReader(uploaded_file)
                 text = ""
                 for page in pdf_reader.pages:
@@ -69,7 +76,13 @@ def main():
                 st.error("Unsupported file format. Please upload a PDF or DOCX file.")
                 return
 
-            # Split text into chunks
+            # split into chunks
+            # text_splitter = CharacterTextSplitter(
+            #     separator="\n",
+            #     chunk_size=1000,
+            #     chunk_overlap=200,
+            #     length_function=len
+            # )
             text_splitter = RecursiveCharacterTextSplitter(
                 chunk_size=1000,
                 chunk_overlap=200,
@@ -77,19 +90,22 @@ def main():
             )
             chunks = text_splitter.split_text(text)
 
-            # Create embeddings
+
+            # create embeddings
             embeddings = OpenAIEmbeddings(disallowed_special=())
             knowledge_base = FAISS.from_texts(chunks, embeddings)
+
             
             st.header("Here's a brief summary of your file:")
             pdf_summary = "Give me a concise summary, use the language that the file is in. "
 
             docs = knowledge_base.similarity_search(pdf_summary)
             
+            
             if 'summary' not in st.session_state or st.session_state.summary is None:
-                with st.spinner('Wait for it...'):
+              with st.spinner('Wait for it...'):
                     try:
-                        st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)
+                            st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)    
                     except Exception as maxtoken_error:
                         # Fallback to the larger model if the context length is exceeded
                         print(maxtoken_error)
@@ -99,27 +115,31 @@ def main():
                             
             st.write(st.session_state.summary)
 
-            # User input for questions
-            user_question = st.text_input("Ask a question about your file:")
+
+            # show user input
+            user_question = st.text_input("Ask a question about your file :")
             if user_question:
                 docs = knowledge_base.similarity_search(user_question)
                 with st.spinner('Wait for it...'):
-                    with get_openai_callback() as cb:
-                        try:
-                            response = chain_qa.run(input_documents=docs, question=user_question)
-                        except Exception as maxtoken_error:
-                            print(maxtoken_error)
-                            response = chain_large_qa.run(input_documents=docs, question=user_question) 
-                        print(cb)
-                        # Show/hide section using st.beta_expander
-                        with st.expander("Used Tokens", expanded=False):
-                            st.write(cb)
+                  with get_openai_callback() as cb:
+                    try:
+                        response = chain_qa.run(input_documents=docs, question=user_question)
+                    except Exception as maxtoken_error:
+                        print(maxtoken_error)
+                        response = chain_large_qa.run(input_documents=docs, question=user_question) 
+                    print(cb)
+                    # show/hide section using st.beta_expander
+                    with st.expander("Used Tokens", expanded=False):
+                       st.write(cb)
                 st.write(response)
                 
         except IndexError:
+            #st.caption("Well, Seems like your PDF doesn't contain any text, try another one.🆖")
             st.error("Please upload another PDF. It seems like this PDF doesn't contain any text.")
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
+
+
 
 if __name__ == '__main__':
     main()
