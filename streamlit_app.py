9bc1962427bcb3218901f2f0691bac902197b1aa
Zhenzhen <zhenzhen1108@hotmail.com>
2023-07-20 15:24:25 +1000

    test callback

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,16 +106,19 @@ def main():
             
             if 'summary' not in st.session_state or st.session_state.summary is None:
               with st.spinner('Wait for it...'):
-                with get_openai_callback() as cb:
+                
                     try:
-                        st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)
+                        
+                            st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)
+                            
                     except Exception as maxtoken_error:
                         # Fallback to the larger model if the context length is exceeded
-                        print(maxtoken_error)
-                        print("pin0")
-                        st.session_state.summary = chain_large.run(input_documents=docs, question=pdf_summary)
-                        print("pin1")
-                    print(cb)
+                        with get_openai_callback() as cb:
+                            print(maxtoken_error)
+                            print("pin0")
+                            st.session_state.summary = chain_large.run(input_documents=docs, question=pdf_summary)
+                            print("pin1")
+                            print(cb)
             st.write(st.session_state.summary)
 
 
