1068a49f76e72258035640c68543e8e7cd781505
Zhenzhen <zhenzhen1108@hotmail.com>
2023-07-31 12:28:13 +1000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def main():
             if 'summary' not in st.session_state or st.session_state.summary is None:
               with st.spinner('Wait for it...'):
                     try:
-                            st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)    
+                        st.session_state.summary = chain.run(input_documents=docs, question=pdf_summary)    
                     except Exception as maxtoken_error:
                         # Fallback to the larger model if the context length is exceeded
                         print(maxtoken_error)
